7d395c91f3a34a2d71f9921a838ab0c42aeefaa5
utesurdako <arktnels@gmail.com>
2024-01-15 22:01:19 +0300

    add: add and edit task

--- app/src/main/java/com/codinginflow/mvvmtodo/ui/addedittask/AddEditTaskViewModel.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/addedittask/AddEditTaskViewModel.kt
@@ -4,13 +4,50 @@ import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.codinginflow.mvvmtodo.data.Task
 import com.codinginflow.mvvmtodo.data.TaskDao
+import com.codinginflow.mvvmtodo.ui.ADD_TASK_RESULT_OK
+import com.codinginflow.mvvmtodo.ui.EDIT_TASK_RESULT_OK
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 
 class AddEditTaskViewModel @ViewModelInject constructor(
     private val taskDao: TaskDao,
     @Assisted private val state: SavedStateHandle
 ) : ViewModel() {
+    private val addEditTaskEventChannel = Channel<AddEditTaskEvent>()
+    val addEditTaskEvent = addEditTaskEventChannel.receiveAsFlow()
+
+    fun onSaveClick() {
+        if (taskName.isBlank()) {
+            showInvalidInputMessage("Name cannot be empty")
+            return
+        }
+
+        if (task != null) {
+            val updatedTask = task.copy(name = taskName, important = taskImportance)
+            updatedTask(updatedTask)
+        } else {
+            val newTask = Task(taskName, taskImportance)
+            createTask(newTask)
+        }
+    }
+
+    private fun showInvalidInputMessage(text: String) = viewModelScope.launch {
+        addEditTaskEventChannel.send(AddEditTaskEvent.ShowInvalidInputMessage(text))
+    }
+
+    private fun createTask(task: Task) = viewModelScope.launch  {
+        taskDao.insert(task)
+        addEditTaskEventChannel.send(AddEditTaskEvent.NavigateBackWithResult(ADD_TASK_RESULT_OK))
+    }
+
+    private fun updatedTask(task: Task) = viewModelScope.launch  {
+        taskDao.update(task)
+        addEditTaskEventChannel.send(AddEditTaskEvent.NavigateBackWithResult(EDIT_TASK_RESULT_OK))
+    }
 
     val task = state.get<Task>("task")
 
@@ -24,5 +61,10 @@ class AddEditTaskViewModel @ViewModelInject constructor(
     set(value) {
         field = value
         state.set("taskImportance", value)
+    }
+
+    sealed class AddEditTaskEvent {
+        data class ShowInvalidInputMessage(val msg: String) : AddEditTaskEvent()
+        data class NavigateBackWithResult(val result: Int) : AddEditTaskEvent()
     }
 }
